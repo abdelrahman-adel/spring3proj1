9117623a24b1e95ce6caefa0244fc6e55f84f623
Abd-Elrahman Adel <abdelrahman-adel@hotmail.com>
2019-08-16 16:56:43 +0200

    lazy comment

--- src/main/java/com/master/spring/spring2proj6/webapp/LoginServlet.java
+++ src/main/java/com/master/spring/spring2proj6/webapp/LoginServlet.java
@@ -12,11 +12,26 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	private UserValidationService userValidationService = new UserValidationService();
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if (userValidationService.isUserValid(username, password)) {
+			request.setAttribute("username", username);
+			request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+		} else {
+			request.setAttribute("errorMessage", "Invalid credentials!!");
+			request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		}
 	}
 
 }
